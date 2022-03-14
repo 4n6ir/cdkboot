55988009cbab0f47ad1c91389b15ca8a3dca1772
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-03-14 00:23:08 +0000

    container

--- cdkboot/cdkboot_lambda.py
+++ cdkboot/cdkboot_lambda.py
@@ -1,14 +1,47 @@
-import aws_cdk as cdk
+from aws_cdk import (
+    Duration,
+    RemovalPolicy,
+    Stack,
+    aws_iam as _iam,
+    aws_lambda as _lambda,
+    aws_logs as _logs,
+    aws_s3 as _s3,
+    aws_ssm as _ssm
+)
+
 from constructs import Construct
+
 from aws_cdk.aws_lambda import Function, InlineCode, Runtime
 
-class CdkbootLambda(cdk.Stack):
+class CdkbootLambda(Stack):
     
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        Function(self, "LambdaFunction", 
-            runtime=Runtime.NODEJS_12_X,
-            handler="index.handler",
-            code=InlineCode("exports.handler = _ => 'Hello, CDK';")
+        role = _iam.Role(
+            self, 'role', 
+            assumed_by = _iam.ServicePrincipal(
+                'lambda.amazonaws.com'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AWSLambdaBasicExecutionRole'
+            )
+        )
+
+        install = _lambda.DockerImageFunction(
+            self, 'install',
+            code = _lambda.DockerImageCode.from_image_asset('install'),
+            timeout = Duration.seconds(900),
+            memory_size = 512,
+            role = role
+        )
+
+        installlogs = _logs.LogGroup(
+            self, 'installlogs',
+            log_group_name = '/aws/lambda/'+install.function_name,
+            retention = _logs.RetentionDays.ONE_DAY,
+            removal_policy = RemovalPolicy.DESTROY
         )
